a2cb31adcc7f194762b629073850cc33211f9d8b
MsPengGL <pglgithub@163.com>
2019-07-12 17:07:19 +0800

    正则表达式的字符介绍

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('1234'.replace(/\d/g, 'X'))
+console.log('1234'.replace(/\d*/g, 'X'))
+console.log('1234'.replace(/\d*?/g, 'X'))
+console.log('1234'.replace(/\d+?/g, 'X'))
+console.log('1234'.replace(/\d??/g, 'X'))
